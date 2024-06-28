af224e6cf2ba8a47a80227aa54114d9ebdd12939
TroJan2001 <lutfenour@gmail.com>
2024-06-28 08:58:24 +0300

    Added currentCycle getter

--- src/Clock.java
+++ src/Clock.java
@@ -1,12 +1,16 @@
 class Clock {
     final private static int CYCLE_TIME = 1000;
-    private static int currentCycle = CYCLE_TIME;
+    private static int currentCycle = CYCLE_TIME/1000;
 
     private Clock() {
     }
 
     public static void tick() {
         currentCycle += currentCycle;
+    }
+
+    public static int getCurrentCycle() {
+        return currentCycle;
     }
 
     public static int getCycleTime() {
