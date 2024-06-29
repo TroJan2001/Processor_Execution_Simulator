0636d2c5a5ff02cdc973c092d56e44d9787e1ec1
TroJan2001 <lutfenour@gmail.com>
2024-06-29 08:44:24 +0300

    Formatting TaskComparator

--- src/TaskComparator.java
+++ src/TaskComparator.java
@@ -5,15 +5,16 @@ public class TaskComparator implements Comparator<Task> {
     final private Random random = new Random();
 
     public int compare(Task o1, Task o2) {
-        if (o1.getPRIORITY() < o2.getPRIORITY())
-            return 1;
-        else if (o1.getPRIORITY() > o2.getPRIORITY()) {
-            return -1;
-        } else if (o1.getEXECUTION_TIME() < o2.getEXECUTION_TIME()) {
-            return 1;
-        } else if (o1.getEXECUTION_TIME() > o2.getEXECUTION_TIME()) {
-            return -1;
-        } else
-            return random.nextInt(2) * 2 - 1;
+        int priorityDiff = o2.getPRIORITY() - o1.getPRIORITY();
+        if (priorityDiff != 0) {
+            return priorityDiff;
+        }
+
+        int executionTimeDiff = o2.getEXECUTION_TIME() - o1.getEXECUTION_TIME();
+        if (executionTimeDiff != 0) {
+            return executionTimeDiff;
+        }
+
+        return random.nextInt(2) * 2 - 1;
     }
 }
